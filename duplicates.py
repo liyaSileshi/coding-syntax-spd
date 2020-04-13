7a9d5844aa80e058f2173a58f089e84be6a7a591
liyaSileshi <liyasileshi@gmail.com>
2020-04-12 17:58:44 -0700

    fixed #1

--- duplicates.py
+++ duplicates.py
@@ -1,25 +1,27 @@
-#given a list of n numbers, determine if it contains any duplicates
+# 1, Given a list of n numbers, determine if it contains any duplicates
 #  n =  [ 2, 4, 3, 5, 7, 7, 9, 9 ]
 # make a set and compare set with length with original list  
 
-
 def has_duplicates(array):
-	s1 = set(array)
+	set1 = set(array)
 
-	if len(s1) == len(array):
+	if len(set1) == len(array):
 		return False
 	return True
 
-# to return duplicates
 
+# to return duplicates  
 def what_duplicates(array):
-	s1 = set()
-	dupes = []
-	
-for item in array:
-		if item not in s1:
-			s1.add(item)
-		else:
-			dupes.append(item)
+    set1 = set() #make an empty set
+    duplicate_list = []
+    for item in array:
+        if item not in set1:
+            set1.add(item) #add to set
+        else:
+            duplicate_list.append(item)
+    return duplicate_list
 
-	return dupes
+
+n =  [ 2, 4, 3, 5, 7, 7, 9, 9 ]
+print(has_duplicates(n))
+print(what_duplicates(n))
